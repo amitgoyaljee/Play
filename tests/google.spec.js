623e72f3a0eeb58c604c6a42543661354641bb5a
Amit Goyal <amitgoyal.t@gmail.com>
2026-08-11 13:12:09 +0530

    ll

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -5,7 +5,6 @@ const Page1 = require('./demo/page1');
 test("my fun",async({page})=>{
     const page1Object=new Page1(page)
      await page.goto('https://www.google.com/');
-     await page1Object.searchTxtFill("ssss") 
-     await page.pause()
+     await page1Object.searchTxtFill("amit") 
 
 });
